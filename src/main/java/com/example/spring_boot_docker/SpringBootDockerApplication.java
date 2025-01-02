6762f644081ec19b3c913efff4c003f466a6d6d1
vinoth-ops <60887738+vinoth-ops@users.noreply.github.com>
2025-01-02 17:21:58 +0530

    Update SpringBootDockerApplication.java

--- src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
+++ src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
@@ -3,7 +3,7 @@ package com.example.spring_boot_docker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication
+@SpringBootApplication
 public class SpringBootDockerApplication {
 
 	public static void main(String[] args) {
