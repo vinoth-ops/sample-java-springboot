9fb1be6aaa2fcab67d1b41491ac1c166838527b7
vinoth-ops <60887738+vinoth-ops@users.noreply.github.com>
2025-01-02 17:10:58 +0530

    Create HelloController.java

---
+++ src/main/java/com/example/spring_boot_docker/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.spring_boot_docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, From Container to World!";
+    }
+}
